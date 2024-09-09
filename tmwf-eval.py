0022c34814545e66ae5856e55ab3c0642ae5540b
M Wrana <wranamichael@gmail.com>
2024-09-09 19:21:58 +0900

    Add files via upload

--- tmwf-eval.py
+++ tmwf-eval.py
@@ -321,7 +321,7 @@ class DataGenerator(object):
                             x.append([x_dire.tolist()[:max_len], x_time.tolist()[:max_len]])
                         else:
                             x.append(
-                                [x_dire.tolist() + [0] * (max_len - tlen), x_time.tolist() + [0] * (max_len - tlen)])
+                                [x_dire.tolist() + [0] * (max_len - tlen - 1), x_time.tolist() + [0] * (max_len - tlen - 1)])
                     else:
                         if tlen >= max_len:
                             x.append(x_dire.tolist()[:max_len])
